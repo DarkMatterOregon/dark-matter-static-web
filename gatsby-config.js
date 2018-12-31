abb0b2d3459531ae225816dcef8b026bb1966383
Greg McKelvey <mckelveygreg@gmail.com>
2018-12-30 16:47:57 -0800

    airtable hookup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -13,6 +17,28 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        // apiKey: process.env.GATSBY_AIRTABLE_API_KEY, // this plugin looks for GATSBY_AIRTABLE_API_KEY automatically if environment variables are required above.
+        tables: [
+          {
+            baseId: `app9Fz1IIOvtsjkDk`, // found in api docs, check the 'show api' button, and is the number '...base('YOUR_BASE ID')
+            tableName: `Sitemap`,
+            // tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            // queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optional
+            // //mapping: { `CASE_SENSITIVE_COLUMN_NAME`: `VALUE_FORMAT` }, // optional, e.g. "text/markdown", "fileNode"
+            // tableLinks: [`CASE`, `SENSITIVE`, `COLUMN`, `NAMES`] // optional, for deep linking to records across tables.
+          },
+         {
+            baseId: `app9Fz1IIOvtsjkDk`,
+            tableName: `Crew`,
+            // tableView: `YOUR_TABLE_VIEW_NAME` // optional
+            // can leave off queryName, mapping or tableLinks if not needed
+       },
+      ]
+        },
+      },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
