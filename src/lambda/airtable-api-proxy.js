62bf719e6259b14119ecd436e0cb955d481d7383
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-10 15:31:04 -0800

    api-key

--- src/lambda/airtable-api-proxy.js
+++ src/lambda/airtable-api-proxy.js
@@ -1,37 +1,36 @@
 const Airtable = require('airtable')
 
-
-const saveContact = async (data) => {
-  return new Promise  ((resolve, reject) => {
-    const { GATSBY_AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env
+const saveContact = async data => {
+  return new Promise((resolve, reject) => {
+    const { AIRTABLE_BASE_ID, GATSBY_AIRTABLE_API_KEY } = process.env
 
     Airtable.configure({
-      GATSBY_AIRTABLE_API_KEY
+      GATSBY_AIRTABLE_API_KEY,
     })
 
     const base = Airtable.base(AIRTABLE_BASE_ID)
     base(data.formName).create(data, err => {
-      if (err) return reject(err);
-      resolve();
+      if (err) return reject(err)
+      resolve()
     })
   })
 }
 
-export async function handler (event) {
+export async function handler(event) {
   try {
     const data = JSON.parse(event.body)
     await saveContact(data)
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Apparently this saved to airtable..."
-      })
+        message: 'Apparently this saved to airtable...',
+      }),
     }
   } catch (error) {
     console.log(error)
     return {
       statusCode: 500,
-      body: error.message
+      body: error.message,
     }
   }
 }
