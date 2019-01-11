19a6993a9762eddaf925207a4161e213c6137793
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-10 16:32:28 -0800

    node process fix

--- src/lambda/airtable-api-proxy.js
+++ src/lambda/airtable-api-proxy.js
@@ -2,7 +2,7 @@ const Airtable = require('airtable')
 
 const saveContact = async data => {
   return new Promise((resolve, reject) => {
-    const { AIRTABLE_BASE_ID, GATSBY_AIRTABLE_API_KEY } = process.env
+    const { AIRTABLE_BASE_ID, GATSBY_AIRTABLE_API_KEY } = process.env.NODE_ENV
 
     Airtable.configure({
       GATSBY_AIRTABLE_API_KEY,
