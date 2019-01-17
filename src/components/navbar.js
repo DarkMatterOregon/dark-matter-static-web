972906bfd1e7703f7dd1234f924ffbe1811e8ea8
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-17 15:20:47 -0800

    fix navbar hover

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'gatsby' 
+import { Link } from 'gatsby'
 
 const NavList = styled.ul`
   list-style: none;
@@ -19,17 +19,18 @@ const NavList = styled.ul`
 const NavLi = styled.li`
   display: inline-block;
   margin-right: 1rem;
-  padding: .3rem;
-  transition: .5s ease;
-
-  &:hover, &:active {
-    background: #FFAA33aa;
-    border-radius: 3px;
-  }
 `
 const StyledLink = styled(Link)`
   color: #2b2b2b;
   text-decoration: none;
+  transition: 0.3s ease;
+  padding: 0.3rem;
+
+  &:hover,
+  &:active {
+    background: #ffaa33aa;
+    border-radius: 3px;
+  }
 `
 const ListLink = props => (
   <NavLi>
