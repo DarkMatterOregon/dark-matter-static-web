b20b24785f43ce866c2bf89b6a8f244e25deb2f5
Greg McKelvey <mckelveygreg@gmail.com>
2019-03-13 11:50:07 -0700

    add qr code and redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,19 @@ const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 exports.onCreateNode = ({ node }) => {
   fmImagesToRelative(node)
 }
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createRedirect } = actions
+  createRedirect({
+    fromPath: '/qr',
+    toPath: '/craftbrew/',
+    isPermanent: true,
+    redirectInBrowser: true,
+  })
+  createRedirect({
+    fromPath: '/qr/',
+    toPath: '/craftbrew/',
+    isPermanent: true,
+    redirectInBrowser: true,
+  })
+}
