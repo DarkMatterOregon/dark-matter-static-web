a73060a0afb9f5f98b3486249357cc056db15974
Greg McKelvey <mckelveygreg@gmail.com>
2019-03-20 13:23:25 -0700

    add content to ar page to pass build

---
+++ src/pages/arTest.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ARTest = () => (
+  <Layout>
+    <div>Future AR test content from qr marker, to be used with Aframe</div>
+  </Layout>
+)
+
+export default ARTest
