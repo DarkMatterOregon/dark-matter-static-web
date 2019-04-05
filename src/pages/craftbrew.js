e053b73c7a6b0c984732cbbe591e532db2f8d83f
Greg McKelvey <mckelveygreg@gmail.com>
2019-04-05 16:57:48 -0700

    add Google analytics to glimmerButton

--- src/pages/craftbrew.js
+++ src/pages/craftbrew.js
@@ -3,7 +3,9 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ContactBrew from '../components/contactBrew'
 import { useStaticQuery, graphql } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import styled from 'styled-components'
+import Img from 'gatsby-image'
 
 const BeerButton = styled.button`
   margin: 0 auto;
@@ -29,7 +31,7 @@ const BeerButton = styled.button`
 `
 
 const CraftBrew = () => {
-  const craftbrewMD = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     {
       markdownRemark(frontmatter: { title: { eq: "Craft Brew" } }) {
         frontmatter {
@@ -37,11 +39,18 @@ const CraftBrew = () => {
         }
         html
       }
+      glimmerButton: file(relativePath: { eq: "glimmer-store.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
     }
   `)
   const [showForm, setShowForm] = useState(false)
   const [submitMessage, setSubmitMessage] = useState()
-
+  console.log(data.glimmerButton.childImageSharp)
   return (
     <Layout>
       <SEO title="Craft Brewery Conference" />
@@ -67,15 +76,19 @@ const CraftBrew = () => {
           <h3 style={{ textAlign: 'center' }}>{submitMessage}</h3>
         )}
       </div>
-      <div
-        dangerouslySetInnerHTML={{ __html: craftbrewMD.markdownRemark.html }}
-      />
-      {/* <div>
-        <a href="http://www.glimmer.tech/">
-          {' '}
-          <img src="" />
-        </a>
-      </div> */}
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      <div>
+        <OutboundLink
+          href="http://www.glimmer.tech/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <Img
+            fluid={data.glimmerButton.childImageSharp.fluid}
+            style={{ maxWidth: 400, margin: '0 auto' }}
+          />
+        </OutboundLink>
+      </div>
     </Layout>
   )
 }
